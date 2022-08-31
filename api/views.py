642e2b0cf54fcd4cba16383415180206cbfd574b
vickison <balivick07@gmail.com>
2022-08-31 09:05:47 -0700

    todo api v1

---
+++ api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from api.serializers import TodoSerializer
+from todo.models import Todo
+
+
+# Create your views here.
+class TodoList(generics.ListAPIView):
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+    def perfom_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
